ceef142f825833c840cc63985644fffb744e66c8
xiaopeng <nimezhu@gmail.com>
2017-09-21 01:19:20 -0400

    add bigbedLarge

--- magic.go
+++ magic.go
@@ -11,6 +11,7 @@ import (
 const BIGWIG_MAGIC = 0x888FFC26
 const BIGBED_MAGIC = 0x8789F2EB
 const HIC_MAGIC = 0x00434948
+const BIGSIZE = 100000000 //100Mb is bigbedLarge
 
 func Magic(uri string) (string, error) {
 	if _, err := os.Stat(filepath.Join(filepath.Dir(uri), "images")); err == nil {
@@ -22,11 +23,18 @@ func Magic(uri string) (string, error) {
 	if err != nil {
 		return "unknown", err
 	}
+	size, err := netio.Size(uri)
+	if err != nil {
+		return "unknown", err
+	}
 	p := make([]byte, 4)
 	f.Read(p)
 	n := binary.LittleEndian.Uint32(p)
 	switch n {
 	case BIGBED_MAGIC:
+		if size > BIGSIZE {
+			return "bigbedLarge", nil
+		}
 		return "bigbed", nil
 	case BIGWIG_MAGIC:
 		return "bigwig", nil
